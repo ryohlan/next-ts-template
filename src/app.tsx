ecab70825e482608730475d3ed96a7ca07b872b3
ryohlan <sunsunryohlan@gmail.com>
2019-02-10 16:47:40 +0900

    update template

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import App, { Container, NextAppContext, DefaultAppIProps } from 'next/app'
 
-interface InitialProps {}
+export interface AppInitialProps {}
 
-export interface AppProps extends InitialProps {}
+export interface AppProps extends AppInitialProps {}
 
 const getInitialProps = async ({
   Component,
   ctx
-}: NextAppContext): Promise<DefaultAppIProps & InitialProps> => {
+}: NextAppContext): Promise<DefaultAppIProps & AppInitialProps> => {
+  const initialProps: AppInitialProps = {}
+
   const pageProps =
-    Component.getInitialProps && (await Component.getInitialProps(ctx))
-  return { pageProps }
+    Component.getInitialProps &&
+    (await Component.getInitialProps({ ...ctx, ...initialProps }))
+
+  return { pageProps, ...initialProps }
 }
 
 class MyApp extends App<AppProps> {
